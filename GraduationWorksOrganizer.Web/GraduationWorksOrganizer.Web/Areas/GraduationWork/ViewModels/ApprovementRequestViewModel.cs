c07967f937307a83a99c48a7bcb46c67b4f1d04b
konstanin0atanasov <konstantin0atanasov@gmail.com>
2020-09-21 17:18:30 +0300

    ... nishto

---
+++ GraduationWorksOrganizer.Web/GraduationWorksOrganizer.Web/Areas/GraduationWork/ViewModels/ApprovementRequestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GraduationWorksOrganizer.Core.ViewModels;
+using System;
+
+namespace GraduationWorksOrganizer.Web.Areas.GraduationWork.ViewModels
+{
+    public class ApprovementRequestViewModel : IAutoMapperViewModel
+    {
+        public MapperConfiguration GetMapperConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
