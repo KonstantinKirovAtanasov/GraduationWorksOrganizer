9b8216c83be94516fda93aa519218487ccffa92c
Konstantin <Konstantin.Kirov.Atanasov@gmail.com>
2020-09-28 23:21:15 +0300

    added new page

---
+++ GraduationWorksOrganizer.Web/GraduationWorksOrganizer.Web/Areas/GraduationWork/Pages/ThesesCorrespondence.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GraduationWorksOrganizer.Web.Areas.GraduationWork.Pages
+{
+    public class ThesesCorrespondenceModel : PageModel
+    {
+        #region Declarations
+
+        #endregion
+
+        #region Constructor
+
+        public ThesesCorrespondenceModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OnGet(int thesesId)
+        {
+        }
+
+        #endregion
+
+    }
+}
