478a0571b4cc22a7efbc19df0456476a3ae4302c
Konstantin <Konstantin.Kirov.Atanasov@gmail.com>
2020-10-06 00:09:36 +0300

    1

--- GraduationWorksOrganizer.Web/GraduationWorksOrganizer.Web/Areas/GraduationWork/Pages/ThesesCorrespondence.cshtml.cs
+++ GraduationWorksOrganizer.Web/GraduationWorksOrganizer.Web/Areas/GraduationWork/Pages/ThesesCorrespondence.cshtml.cs
@@ -1,6 +1,7 @@
 using GraduationWorksOrganizer.Database.Models;
 using GraduationWorksOrganizer.Database.Services.BaseServices;
 using GraduationWorksOrganizer.Web.Areas.GraduationWork.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,9 +25,23 @@ namespace GraduationWorksOrganizer.Web.Areas.GraduationWork.Pages
 
         #endregion
 
+        public class InputModel
+        {
+            public int DateDefenceEventId { get; set; }
+
+            public string Description { get; set; }
+
+            public int UserEntryId { get; set; }
+        }
+
         #region Properties
 
+        [BindProperty]
+        public InputModel Input { get; set; }
+
         public IEnumerable<FileViewModel> Files { get; set; }
+
+        public int UserEntryId { get; set; }
 
         #endregion
 
@@ -34,15 +49,34 @@ namespace GraduationWorksOrganizer.Web.Areas.GraduationWork.Pages
 
         public void OnGet(int userEntryId)
         {
-            Files = _filesDbService.GetQuery()
-                                   .Where(f => f.UserEntryId == userEntryId)
-                                   .Select(f => new FileViewModel()
-                                   {
-                                       Name = f.FileName,
-                                       Size = f.Content.Length / 1024,
-                                       Id = f.Id
-                                   });
+            SelectFiles(userEntryId);
+            UserEntryId = userEntryId;
         }
+
+        public void OnPostProceed()
+        {
+            SelectFiles(Input.UserEntryId);
+            UserEntryId = Input.UserEntryId;
+        }
+
+        public void OnPostCancel()
+        {
+            SelectFiles(Input.UserEntryId);
+            UserEntryId = Input.UserEntryId;
+        }
+
+        private void SelectFiles(int userEntryId)
+        {
+            Files = _filesDbService.GetQuery()
+                                               .Where(f => f.UserEntryId == userEntryId)
+                                               .Select(f => new FileViewModel()
+                                               {
+                                                   Name = f.FileName,
+                                                   Size = f.Content.Length / 1024,
+                                                   Id = f.Id
+                                               });
+        }
+
 
         #endregion
 
